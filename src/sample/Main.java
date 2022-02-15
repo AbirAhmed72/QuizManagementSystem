b99f0f752aab0a804c8a6fb39162185f86e4ffc4
AbirAhmed72 <abirahmed14872@gmail.com>
2022-02-15 22:43:21 +0600

    5th: Tweaked the primaryStage

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,23 +4,35 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
+    private static Stage stg;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        stg = primaryStage;
+        primaryStage.setResizable(false);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Welcome to QuizBiz");
+        primaryStage.setScene(new Scene(root, 900, 600, Color.BLACK));
         primaryStage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
     }
 
 
     public static void main(String[] args) {
 
         System.out.println("Git parbo in sha Allah");
-        launch(args);
         System.out.println("Git parsi Alhamdulillah. 4th commit here");
+        launch(args);
     }
 }
