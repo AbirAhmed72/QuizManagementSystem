1577c03cef1201dd796f67e1c6a2ccba61eb52a2
AbirAhmed72 <abirahmed14872@gmail.com>
2022-03-17 14:15:24 +0600

    18th : Added the UI of adding Questions

--- src/SPL_1/AddQuestionsController.java
+++ src/SPL_1/AddQuestionsController.java
@@ -1,4 +1,49 @@
 package SPL_1;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class AddQuestionsController {
+
+    @FXML
+    private TextField questionsField;
+    @FXML
+    private TextField firstOptionField;
+    @FXML
+    private TextField secondOptionField;
+    @FXML
+    private TextField thirdOptionField;
+    @FXML
+    private TextField fourthOptionField;
+    @FXML
+    private TextField correctAnswerField;
+    @FXML
+    private Button addQuestionsBack;
+    @FXML
+    private Button saveQuestions;
+    @FXML
+    private Label questionsErrorLabel;
+
+    public void addQuestionsBackButton(ActionEvent event) throws IOException {
+        Main m = new Main();
+        m.changeScene("adminDashboard.fxml");
+    }
+    public void saveQuestionsButton(ActionEvent event) throws IOException {
+
+
+        if(questionsField.getText().isEmpty() || firstOptionField.getText().isEmpty() ||
+                secondOptionField.getText().isEmpty() || thirdOptionField.getText().isEmpty() ||
+                fourthOptionField.getText().isEmpty() || correctAnswerField.getText().isEmpty()){
+
+            questionsErrorLabel.setText("Please Fillout All Cells!");
+        }
+
+        else questionsErrorLabel.setText("Questions Saved Successfully!");
+
+    }
 }
